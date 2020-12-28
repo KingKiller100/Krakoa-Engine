4e4796658a4cdc82ace03f9b75c8e396ecdd05f4
KingKayDee <kwekudeheer@ymail.com>
2020-12-28 22:35:21 +0000

    Added more to message box enum

--- Krakoa/Source Files/Debug/Windows/DebugWindows.cpp
+++ Krakoa/Source Files/Debug/Windows/DebugWindows.cpp
@@ -7,28 +7,30 @@
 #include <Utility/String/kToString.hpp>
 #include <Utility/Debug/Source/kSourceInfoToString.hpp>
 
-#ifdef KRAKOA_OS_WINDOWS
+#if KRAKOA_OS_WINDOWS
 
 #include <Windows.h>
 
 namespace krakoa::debug::windows
 {
 	ENUM_CLASS(MessageBoxResponse, long,
-		MSG_CANCEL = 2,
+		MSG_OKAY = 1,
+		MSG_CANCEL,
 		MSG_ABORT,
 		MSG_RETRY,
 		MSG_IGNORE,
 		MSG_YES,
-		MSG_NO
+		MSG_NO,
+		MSG_TRY_AGAIN = 10,
+		MSG_CONTINUE
 	);
 
 	void RaiseNoticeImpl(const std::string_view& msg, const klib::SourceInfo& sourceInfo)
 	{
 		const auto caption = klib::ToString("{0}\n Source: {1}", msg, sourceInfo);
 
-		const MessageBoxResponse response = static_cast<MessageBoxResponse>(
-			MessageBoxA(nullptr, caption.data(), "ERROR OCCURRED", MB_YESNOCANCEL)
-			);
+		const auto ret = MessageBoxA(nullptr, caption.data(), "ERROR OCCURRED", MB_COMPOSITE);
+		const auto response = static_cast<MessageBoxResponse>(ret);
 		const auto valueStr = response.ToString();
 		std::cout << valueStr;
 	}

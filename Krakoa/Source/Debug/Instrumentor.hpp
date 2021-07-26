e1f61efec9818ad483f3fde920a092ea7d7c5c92
King Kay Dee <kwekudeheer@ymail.com>
2021-07-26 20:57:07 +0100

    Fixed profiler so it can compile again

--- Krakoa/Source/Debug/Instrumentor.hpp
+++ Krakoa/Source/Debug/Instrumentor.hpp
@@ -44,14 +44,14 @@ namespace krakoa
 }
 
 #ifdef KRAKOA_PROFILE
-#define KRK_PROFILE_SESSION_BEGIN(name, filepath) krakoa::Instrumentor::Get().BeginSession(name, filepath)
-#define KRK_PROFILE_SESSION_END() krakoa::Instrumentor::Get().EndSession()
-#define KRK_PROFILE_SCOPE(name) klib::kProfiler::Profiler<std::time_t> timer##__LINE__(name, [&](const klib::kProfiler::ProfilerResult& result) { krakoa::Instrumentor::Get().WriteProfile(result); })
-#define KRK_PROFILE_FUNCTION()  KRK_PROFILE_SCOPE(__FUNCSIG__)
+#	define KRK_PROFILE_SESSION_BEGIN(name, filepath) krakoa::Instrumentor::Get().BeginSession(name, filepath)
+#	define KRK_PROFILE_SESSION_END() krakoa::Instrumentor::Get().EndSession()
+#	define KRK_PROFILE_SCOPE(name) klib::kProfiler::Profiler<std::time_t> timer##__LINE__(name, [&](const klib::kProfiler::ProfilerResult<std::time_t>& result) { krakoa::Instrumentor::Get().WriteProfile(result); })
+#	define KRK_PROFILE_FUNCTION()  KRK_PROFILE_SCOPE(__FUNCSIG__)
 #else
-#define KRK_PROFILE_SESSION_BEGIN(name, filepath) 
-#define KRK_PROFILE_SESSION_END() 
-#define KRK_PROFILE_SCOPE(name) 
-#define KRK_PROFILE_FUNCTION()  
+#	define KRK_PROFILE_SESSION_BEGIN(name, filepath) 
+#	define KRK_PROFILE_SESSION_END() 
+#	define KRK_PROFILE_SCOPE(name) 
+#	define KRK_PROFILE_FUNCTION()  
 #endif
 

323e5770bb3c4feae5571a27c31135ce4c63296b
King Kay Dee <kwekudeheer@ymail.com>
2021-11-07 04:23:15 +0000

    Import wrapper to pass name of import around with function

--- Krakoa/Source/Platform/OperatingSystem/Library/iOSLibrary.hpp
+++ Krakoa/Source/Platform/OperatingSystem/Library/iOSLibrary.hpp
@@ -4,11 +4,68 @@
 
 namespace krakoa::os::library
 {
+	template <typename Import>
+	struct ImportWrapper
+	{
+	public:
+		using Import_t = Import;
+
+		constexpr ImportWrapper( const std::string_view& name, void* ip )
+			: id( name )
+			, imp( static_cast<Import*>( ip ) )
+		{ }
+
+		USE_RESULT constexpr std::string_view GetID() const noexcept
+		{
+			return id;
+		}
+
+		constexpr Import* GetImport() const
+		{
+			return imp;
+		}
+
+		template <typename ...Args>
+		constexpr decltype(auto) operator()( Args&& ...args ) const
+		{
+			return imp( std::forward<Args>( args )... );
+		}
+
+		USE_RESULT constexpr operator bool() const
+		{
+			return imp;
+		}
+
+		USE_RESULT constexpr bool operator==( std::nullptr_t ) const
+		{
+			return imp == nullptr;
+		}
+
+		USE_RESULT constexpr bool operator!=( std::nullptr_t ) const
+		{
+			return imp != nullptr;
+		}
+
+		USE_RESULT constexpr bool operator==( const ImportWrapper& other ) const
+		{
+			return imp == other.imp;
+		}
+
+		USE_RESULT constexpr bool operator!=( const ImportWrapper& other ) const
+		{
+			return imp != other.imp;
+		}
+
+	private:
+		std::string id;
+		Import* imp;
+	};
+
 	class iOSLibrary
 	{
 	public:
 		template <typename T>
-		using ImportPtr_t = std::remove_pointer_t<T>*;
+		using ImportPtr_t = std::remove_pointer_t<T>;
 
 	public:
 		virtual ~iOSLibrary() = default;
@@ -19,7 +76,7 @@ namespace krakoa::os::library
 		template <typename T>
 		auto Import( const std::string_view& id )
 		{
-			return static_cast<ImportPtr_t<T>>( ImportImpl( id.data() ) );
+			return ImportWrapper<ImportPtr_t<T>>( id, ImportImpl( id.data() ) );
 		}
 
 	protected:

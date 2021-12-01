7245eefcb8df16aecf6e34d933393210146f812b
King Kay Dee <kwekudeheer@ymail.com>
2021-12-01 14:16:13 +0000

    Simplifying logging api and exposure for rest of engine
    Next need to update uses

--- Krakoa/Source/Logging/EngineLogger.cpp
+++ Krakoa/Source/Logging/EngineLogger.cpp
@@ -3,96 +3,149 @@
 
 #include "../Config/GlobalConfig.hpp"
 
+#include <Utility/Logging/Destinations/kFileLogger.hpp>
+#include <Utility/Logging/Destinations/kConsoleLogger.hpp>
+#include <Utility/Logging/Destinations/kDebugOutputWindowLogger.hpp>
 #include <Utility/FileSystem/kFileSystem.hpp>
 #include <Utility/Calendar/kCalendar.hpp>
 
 namespace krakoa
 {
 	using namespace filesystem;
-	
+
+	namespace
+	{
+		std::shared_ptr<klib::FileLogger> g_FileLogger;
+		std::shared_ptr<klib::ConsoleLogger> g_ConsoleLogger;
+		std::shared_ptr<klib::DebugOutputWindowLogger> g_DebugOutputWindowLogger;
+	}
+
 	Solo_Ptr<klib::Logging> EngineLogger::pLogger;
 
-	void EngineLogger::CoreInit(const std::string_view& initMsg)
+	void EngineLogger::CoreInit( const std::string_view& initMsg )
 	{
-		if (pLogger)
+		if ( pLogger )
 			return;
 
-		std::filesystem::path dir(klib::kFileSystem::GetExeDirectory());
+		std::filesystem::path dir( klib::kFileSystem::GetExeDirectory() );
 		dir /= "Logs\\";
 
 		constexpr auto name = "Krakoa";
 		constexpr auto filename = "KrakoaEngine";
 		constexpr auto extension = ".log";
 
-		const auto path = dir / (std::string(filename) + extension);
+		const auto path = dir / ( std::string( filename ) + extension );
 
-		const auto padding = std::string(72, '*');
-		const auto spacing = std::string(12, ' ');
+		const auto padding = std::string( 72, '*' );
+		const auto spacing = std::string( 12, ' ' );
 
-		const klib::GregorianCalendar now(klib::CalendarInfoSourceType::LOCAL);
-		const auto timeStr = now.GetTime().ToString(klib::Time::MILLIS);
-		const auto dateStr = now.GetDate().ToString("mmm ddd yyyy");
+		const klib::GregorianCalendar now( klib::CalendarInfoSourceType::LOCAL );
+		const auto timeStr = now.GetTime().ToString( klib::Time::MILLIS );
+		const auto dateStr = now.GetDate().ToString( "mmm ddd yyyy" );
 		const auto stamp = spacing + dateStr + spacing + timeStr + spacing;
 
-		pLogger = Make_Solo<klib::Logging>(path, name);
+		pLogger = Make_Solo<klib::Logging>();
+		g_FileLogger = pLogger->AddDestination<klib::FileLogger>( path );
+		g_ConsoleLogger = pLogger->AddDestination<klib::ConsoleLogger>();
+		g_DebugOutputWindowLogger = pLogger->AddDestination<klib::DebugOutputWindowLogger>();
 
-		pLogger->SetCacheMode(false);
-		pLogger->GetConsole().SetDebugStringOutput(true);
+		pLogger->SetCacheMode( false );
 
-		pLogger->AddRaw(padding);
-		pLogger->AddRaw(stamp);
-		pLogger->AddRaw(initMsg);
-		pLogger->AddRaw(padding);
+		g_FileLogger->Open();
+		g_ConsoleLogger->Open();
+		g_DebugOutputWindowLogger->Open();
+
+		pLogger->AddRaw( padding );
+		pLogger->AddRaw( stamp );
+		pLogger->AddRaw( initMsg );
+		pLogger->AddRaw( padding );
 
 		pLogger->AddRaw();
 	}
 
-	void EngineLogger::SetMinimumLogLevelUsingConfig()
+	void EngineLogger::Raw( std::string_view message )
 	{
-		const auto logLevelStr = configurations::GetConfiguration<std::string_view>("Logging", "Level");
-		const auto min_level = klib::LogLevel::FromString(klib::ToUpper(logLevelStr));
-		pLogger->SetMinimumLoggingLevel(min_level);
+		pLogger->AddRaw( message );
 	}
 
-	void EngineLogger::RemoveIfTooOldFile()
+	void EngineLogger::Trace( std::string_view profile, std::string_view message )
 	{
-		const auto logPath = pLogger->GetFile().GetPath();
-		
-		const auto entry = std::filesystem::directory_entry(logPath);
+		pLogger->AddEntry( klib::LogLevel::TRC, klib::LogProfile( profile ), klib::LogMessage( message ) );
+	}
 
-		if (!entry.exists())
-			return;
+	void EngineLogger::Debug( std::string_view profile, std::string_view message )
+	{
+		pLogger->AddEntry( klib::LogLevel::DBG, klib::LogProfile( profile ), klib::LogMessage( message ) );
+	}
 
-		auto& fileLogger = pLogger->GetFile();
+	void EngineLogger::Normal( std::string_view profile, std::string_view message )
+	{
+		pLogger->AddEntry( klib::LogLevel::NRM, klib::LogProfile( profile ), klib::LogMessage( message ) );
+	}
 
-		fileLogger.Close(false);
-		
-		const auto maxBytes = configurations::GetConfiguration<size_t>("Logging", "MaxBytes");
-		const auto fileSize = entry.file_size();
-		const auto tooBig = fileSize > maxBytes;
+	void EngineLogger::Info( std::string_view profile, std::string_view message )
+	{
+		pLogger->AddEntry( klib::LogLevel::INF, klib::LogProfile( profile ), klib::LogMessage( message ) );
+	}
 
-		const auto maxDays = configurations::GetConfiguration<size_t>("Logging", "MaxDays");
-		const auto lastWrite = entry.last_write_time();
-		const auto now = std::filesystem::_File_time_clock::now();
-		const auto timeSinceLastWrite = now - lastWrite;
-		const auto totalHours = std::chrono::duration_cast<std::chrono::hours>(timeSinceLastWrite);
-		const auto totalDays = totalHours / 24.0;
-		const auto tooOld = maxDays < totalDays.count();
+	void EngineLogger::Warn( std::string_view profile, std::string_view message )
+	{
+		pLogger->AddEntry( klib::LogLevel::WRN, klib::LogProfile( profile ), klib::LogMessage( message ) );
+	}
 
-		if (tooBig || tooOld)
-			klib::Remove(entry.path());
-		
-		fileLogger.Open();
+	void EngineLogger::Error( std::string_view profile, std::string_view message, klib::SourceInfo sourceInfo )
+	{
+		pLogger->AddEntry( klib::LogLevel::ERR, klib::LogProfile( profile ), klib::LogMessage( klib::ToString( "{0}\n{1}", message, sourceInfo ) ) );
+	}
+
+	void EngineLogger::Banner( const klib::LogMessage& message, const std::string_view& frontPadding, const std::string_view& backPadding, std::uint16_t paddingCount )
+	{
+		pLogger->AddBanner( message, frontPadding, backPadding, paddingCount );
 	}
 
 	void EngineLogger::ShutDown()
 	{
-		GetLogger().AddRaw(R"(
+		GetLogger().AddRaw( R"(
 ************************************************************************
                               Logging Concluded
 ************************************************************************)"
-);
-		pLogger->FinalOutput(false);
+		);
+	}
+
+	void EngineLogger::SetMinimumLogLevelUsingConfig()
+	{
+		const auto logLevelStr = configurations::GetConfiguration<std::string_view>( "Logging", "Level" );
+		const auto min_level = klib::LogLevel::FromString( klib::ToUpper( logLevelStr ) );
+		pLogger->SetGlobalLevel( min_level );
+	}
+
+	void EngineLogger::RemoveIfTooOldFile()
+	{
+		const auto logPath = g_FileLogger->GetPath();
+
+		const auto entry = std::filesystem::directory_entry( logPath );
+
+		if ( !entry.exists() )
+			return;
+
+		g_FileLogger->Close();
+
+		const auto maxBytes = configurations::GetConfiguration<size_t>( "Logging", "MaxBytes" );
+		const auto fileSize = entry.file_size();
+		const auto tooBig = fileSize > maxBytes;
+
+		const auto maxDays = configurations::GetConfiguration<size_t>( "Logging", "MaxDays" );
+		const auto lastWrite = entry.last_write_time();
+		const auto now = std::filesystem::_File_time_clock::now();
+		const auto timeSinceLastWrite = now - lastWrite;
+		const auto totalHours = std::chrono::duration_cast<std::chrono::hours>( timeSinceLastWrite );
+		const auto totalDays = totalHours / 24.0;
+		const auto tooOld = maxDays < totalDays.count();
+
+		if ( tooBig || tooOld )
+			klib::Remove( entry.path() );
+
+		g_FileLogger->Open();
 	}
 
 	klib::Logging& EngineLogger::GetLogger()
--- Krakoa/Source/Logging/EngineLogger.hpp
+++ Krakoa/Source/Logging/EngineLogger.hpp
@@ -11,58 +11,45 @@
 
 namespace krakoa
 {
-	namespace configurations
-	{
-		class ConfigValueMap;
-	}
-
 	class KRAKOA_API EngineLogger
 	{
 	public:
 		// Engine side
-		static void CoreInit(const std::string_view& initMsg);
+		static void CoreInit( const std::string_view& initMsg );
 		static klib::kLogs::Logging& GetLogger();
 		static void ShutDown();
 
 		static void SetMinimumLogLevelUsingConfig();
 		static void RemoveIfTooOldFile();
+
+		static void Raw( std::string_view message );
+
+		static void Trace( std::string_view profile, std::string_view message );
+		static void Debug( std::string_view profile, std::string_view message );
+		static void Normal( std::string_view profile, std::string_view message );
+		static void Info( std::string_view profile, std::string_view message );
+		static void Warn( std::string_view profile, std::string_view message );
+		static void Error( std::string_view profile, std::string_view message, klib::SourceInfo sourceInfo );
 		
+		static void Banner(
+			const klib::LogMessage& message
+			, const std::string_view& frontPadding
+			, const std::string_view& backPadding
+			, std::uint16_t paddingCount
+		);
+
 	private:
 		static Solo_Ptr<klib::kLogs::Logging> pLogger;
 	};
 }
 
-// Initializer
-#define KRK_LOG_LVL_TRC                                            ::klib::kLogs::LogLevel::TRC
-#define KRK_LOG_LVL_DBG                                            ::klib::kLogs::LogLevel::DBG
-#define KRK_LOG_LVL_NRM                                            ::klib::kLogs::LogLevel::NRM
-#define KRK_LOG_LVL_INF                                            ::klib::kLogs::LogLevel::INF
-#define KRK_LOG_LVL_WRN                                            ::klib::kLogs::LogLevel::WRN
-#define KRK_LOG_LVL_ERR                                            ::klib::kLogs::LogLevel::ERR
-
-
 // ENGINE SIDE Log Macros
 #define KRK_INIT_LOGS(msg)                                          ::krakoa::EngineLogger::CoreInit(msg)
-#define KRK_SET_LOG_MIN(minLvl)                                     ::krakoa::EngineLogger::GetLogger().SetMinimumLoggingLevel(minLvl)
-#define KRK_GET_LOG_PATH()                                          ::krakoa::EngineLogger::GetLogger().GetFile().GetPath()
-#define KRK_TOGGLE_LOGGING()                                        ::krakoa::EngineLogger::GetLogger().ToggleLoggingEnabled()
-#define KRK_DISABLE_FILE_OUTPUT()                                   ::krakoa::EngineLogger::GetLogger().SetCacheMode(true)
-#define KRK_ENABLE_FILE_OUTPUT()                                    ::krakoa::EngineLogger::GetLogger().SetCacheMode(false)
-#define KRK_CHANGE_LOGS_DESTINATION(destination)                    ::krakoa::EngineLogger::GetLogger().GetFile().SetPath(destination)
-#define KRK_CHANGE_LOGS_FILENAME(name)                              ::krakoa::EngineLogger::GetLogger().GetFile().SetFileName(name)
-#define KRK_LOG_RAW(msg)                                            ::krakoa::EngineLogger::GetLogger().AddRaw(msg)
-#define KRK_TRC(msg)                                                ::krakoa::EngineLogger::GetLogger().AddEntry(KRK_LOG_LVL_TRC, ::klib::kLogs::LogMessage(msg))
-#define KRK_DBG(msg)                                                ::krakoa::EngineLogger::GetLogger().AddEntry(KRK_LOG_LVL_DBG, ::klib::kLogs::LogMessage(msg))
-#define KRK_NRM(msg)                                                ::krakoa::EngineLogger::GetLogger().AddEntry(KRK_LOG_LVL_NRM, ::klib::kLogs::LogMessage(msg))
-#define KRK_INF(msg)                                                ::krakoa::EngineLogger::GetLogger().AddEntry(KRK_LOG_LVL_INF, ::klib::kLogs::LogMessage(msg))
-#define KRK_WRN(msg)                                                ::krakoa::EngineLogger::GetLogger().AddEntry(KRK_LOG_LVL_WRN, ::klib::kLogs::LogMessage(msg, SOURCE_INFO()))
-#define KRK_ERR(msg)                                                ::krakoa::EngineLogger::GetLogger().AddEntry(KRK_LOG_LVL_ERR, ::klib::kLogs::LogMessage(msg, SOURCE_INFO()))
-#define KRK_BANNER(msg, descriptor, frontPad, backPad, count)       ::krakoa::EngineLogger::GetLogger().AddBanner(descriptor, msg, frontPad, backPad, count)
-#define KRK_LOG(descriptor, msg)                                    ::krakoa::EngineLogger::GetLogger().AddBanner(descriptor, msg, "", "", 0)
-#define KRK_SUSPEND()                                               ::krakoa::EngineLogger::GetLogger().GetFile().Close(false)
-#define KRK_RESUME()                                                ::krakoa::EngineLogger::GetLogger().GetFile().Open()
+#define KRK_LOG_RAW(msg)                                            ::krakoa::EngineLogger::Raw(msg)
+#define KRK_TRC(profile, msg)                                       ::krakoa::EngineLogger::Trace(profile, msg)
+#define KRK_DBG(profile, msg)                                       ::krakoa::EngineLogger::Debug(profile, msg)
+#define KRK_NRM(profile, msg)                                       ::krakoa::EngineLogger::Normal(profile, msg)
+#define KRK_INF(profile, msg)                                       ::krakoa::EngineLogger::Info(profile, msg)
+#define KRK_WRN(profile, msg)                                       ::krakoa::EngineLogger::Warn(profile, msg)
+#define KRK_ERR(profile, msg)                                       ::krakoa::EngineLogger::Error(profile, msg, SOURCE_INFO()))
 #define KRK_LOG_END()                                               ::krakoa::EngineLogger::ShutDown()
-#define KRK_LOG_GET_LAST()                                          ::krakoa::EngineLogger::GetLogger().GetLastCachedEntry()
-#define KRK_LOG_ERASE_PREV(numOfPrevEntries)                        ::krakoa::EngineLogger::GetLogger().ErasePrevious(numOfPrevEntries)
-#define KRK_LOG_CLEAR()                                             ::krakoa::EngineLogger::GetLogger().ClearCache()
-
